fbccbf43167cc8d524b3355ba08348299c0cdf08
iamalwaysuncomfortable <zaphrod.beeblebrox@gmail.com>
2018-10-18 13:15:55 -0500

    added script for deployment to any network

--- Demo_Contracts/deploy.js
+++ Demo_Contracts/deploy.js
@@ -1,21 +1,31 @@
+require('dotenv').config();
 const Web3 = require('web3');
 const solCompiler = require('./compile');
-const HDWalletProvider = require("truffle-hdwallet-provider");
-const provider = new HDWalletProvider(require('../set_envars')['mnemonic'], require('../set_envars')['providers'][0]);
-const web3 = new Web3(provider);
-const initialMessage = "Deployed";
 let result;
 
-deploy = async () => {
-    const {interface, bytecode} = solCompiler.compileContract('Lottery');
+__deployContract__ = (web3, contractName) => {
+    let {interface, bytecode} = solCompiler.compileContract(contractName);
     accounts = await web3.eth.getAccounts();
     web3.eth.getBalance(accounts[0]).then(console.log);
     console.log(accounts[0]);
     result = await new web3.eth.Contract(JSON.parse(interface)).deploy({data:bytecode})
         .send({from:accounts[0], gas:'1000000'});
+    return result
+}
 
-    console.log(result.options.address);
+truffleDeployWithEnvars = async (contractName) => {
+    let HDWalletProvider = require("truffle-hdwallet-provider");
+    let provider = new HDWalletProvider(process.env.MNEMONIC, process.env.PROVIDER_ID);
+    let web3 = new Web3(provider);
+    return __deployContract__(web3, contractName)
     };
 
-deploy();
-module.exports = result;
+truffleDeployWithCustomConfig = async (contractName, mnemonic, provider_id) => {
+    let HDWalletProvider = require("truffle-hdwallet-provider");
+    let provider = new HDWalletProvider(mnemonic, provider_id);
+    let web3 = new Web3(provider);
+    return __deployContract__(web3, contractName)
+};
+
+module.exports.truffleDeployWithEnvars = truffleDeployWithEnvars;
+module.exports.truffleDeployWithCustomConfig = truffleDeployWithCustomConfig;
